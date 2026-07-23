dfa90c4130d4bd02321b93932363f1f5c0414481
Ranjit singh <rs0158439@gmail.com>
2026-07-24 02:33:50 +0530

    fix: remove accidental less-than typo and close main content div in user create layout

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.app')
 
 @section('content')
-  <<div class="main-content-container overflow-hidden">
+  <div class="main-content-container overflow-hidden">
                     <div class="d-flex justify-content-between align-items-center flex-wrap gap-2 mb-4 mt-1">
                         <h3 class="mb-0">Create User</h3>
 
@@ -135,4 +135,5 @@
       </form>
 
     </div>
+  </div>
   @endsection
